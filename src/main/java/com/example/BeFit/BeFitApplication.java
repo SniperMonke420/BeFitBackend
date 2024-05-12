a52918a516736a7bd0ad212e6ad153e67e3e672e
LorakV9 <121059447+LorakV9@users.noreply.github.com>
2024-05-13 00:14:19 +0200

    demo

--- src/main/java/com/example/BeFit/BeFitApplication.java
+++ src/main/java/com/example/BeFit/BeFitApplication.java
@@ -1,13 +1,29 @@
 package com.example.BeFit;
 
+import com.befit.befit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
+
 public class BeFitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeFitApplication.class, args);
 		//asdasdasvcbcvbc
+	}
+	@GetMapping
+	public List<befit> hello(){
+		return List.of(new befit(
+				1L,
+				"asd",
+				"asd",
+				"asd"
+		));
 	}
 }
